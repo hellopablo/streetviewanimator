9b9642fe093899fb45c083657605933bac1900f7
Pablo de la Peña <p@shedcollective.org>
2015-06-22 21:18:14 +0100

    Beginnings of a demo page / getting this working

---
+++ demo/assets/js/scenes.js
@@ -0,0 +1,51 @@
+window.demoScenes = {
+    "frameWidth": 600,
+    "frameHeight": 338,
+    "frameRate": 12,
+    "scenes": [
+        {
+            "type": "AUTO",
+            "start": "E2 7LF",
+            "end": "E3 3GQ",
+            "duration": 5,
+            "target": {
+                "lat": null,
+                "lng": null
+            },
+            "pitch": null,
+            "fov": null,
+            "frames": []
+        },
+        {
+            "type": "AUTO",
+            "start": "E3 3GQ",
+            "end": "E2 7LF",
+            "duration": 15,
+            "target": {
+                "lat": null,
+                "lng": null
+            },
+            "pitch": null,
+            "fov": null,
+            "frames": []
+        },
+        {
+            "type": "MANUAL",
+            "start": null,
+            "end": null,
+            "duration": null,
+            "target": {
+                "lat": null,
+                "lng": null
+            },
+            "pitch": null,
+            "fov": null,
+            "frames": [
+                "http://lorempixel.com/600/338",
+                "http://lorempixel.com/600/338",
+                "http://lorempixel.com/600/338",
+                "http://lorempixel.com/600/338"
+            ]
+        }
+    ]
+};
