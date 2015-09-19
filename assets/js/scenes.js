b4e1314df6e2898e098e0eb5af949c5b95c98b23
Pablo de la Peña <p@shedcollective.org>
2015-09-19 14:09:57 +0100

    Updated streetviewanimator to v1.1.0 and included "sweep" scenes in demo

--- assets/js/scenes.js
+++ assets/js/scenes.js
@@ -1,6 +1,14 @@
 window.demo = [
     {
         "id": "scene-1",
+        "type": "SWEEP",
+        "origin": "E2 7LF",
+        "duration": 3,
+        "startHeading": 75,
+        "finishHeading": 250
+    },
+    {
+        "id": "scene-2",
         "type": "AUTO",
         "origin": "E2 7LF",
         "destination": "E3 3GQ",
